7558c067f07c971e16cf94601a75fe052c2b0d1e
luke.rodham <luke.rodham@sipsynergy.co.uk>
2017-03-20 15:43:29 +0000

    NO_ISSUE: stopped human id generation from overflowing

--- utils/id-generator.go
+++ utils/id-generator.go
@@ -1,16 +1,25 @@
 package utils
 
 import (
-	"strings"
 	"math/rand"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
-func GenerateId(prefix string) string {
-
-	i := rand.Intn(9999999999)
-
-	s := prefix + "-" + strconv.Itoa(i)
+// GenerateHumanID will create a human id.
+// It returns the human id string.
+func GenerateHumanID(prefix string) string {
+	i := rand.Int63()
+	s := prefix + "-" + strconv.FormatInt(i, 10)[:10]
 
 	return strings.ToUpper(s)
+}
+
+// ValidateHumanID validates the given string.
+// returns true or false.
+func ValidateHumanID(humanID string) bool {
+	re := regexp.MustCompile(`^[A-Z]+-\d+`)
+
+	return re.MatchString(humanID)
 }
