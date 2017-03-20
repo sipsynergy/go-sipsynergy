0762555ae27c09618295fc18200d48c93db881dd
luke.rodham <luke.rodham@sipsynergy.co.uk>
2017-03-20 12:01:37 +0000

    NO_ISSUE: updated some subpackages and added some

--- utils/id-generator.go
+++ utils/id-generator.go
@@ -1,16 +1,18 @@
 package utils
 
 import (
-	"strings"
 	"math/rand"
 	"strconv"
+	"strings"
+	"time"
 )
 
-func GenerateId(prefix string) string {
-
-	i := rand.Intn(9999999999)
-
+// GenerateHumanID will create a human id.
+// It returns the human id string and the associated integer.
+func GenerateHumanID(prefix string) (string, int64) {
+	rand.Seed(time.Now().Unix())
+	i := rand.Intn(99999999)
 	s := prefix + "-" + strconv.Itoa(i)
 
-	return strings.ToUpper(s)
+	return strings.ToUpper(s), int64(i)
 }
