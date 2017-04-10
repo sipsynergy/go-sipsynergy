256923b7e9a92c2fe577d2c530c15113a44fd413
ryan.hancock <ryan.hancock@sipsynergy.co.uk>
2017-04-10 09:46:56 +0100

    DVT-75: added comment and put type on the key var

--- utils/env_helper.go
+++ utils/env_helper.go
@@ -2,7 +2,9 @@ package utils
 
 import "os"
 
-func GetEnv(key, fallback string) string {
+// GetEnv will get the environment value from OS, if empty will use the fallback given.
+// returns string.
+func GetEnv(key string, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return fallback
