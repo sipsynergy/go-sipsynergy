3db2379505527a423fdb1a1cfc0bb2f1dfe55bce
ryan.hancock <ryan.hancock@sipsynergy.co.uk>
2017-04-10 10:08:41 +0100

    DVT-75: further testing added, changed fallback to interface to allow for more value types to be used

--- utils/env-helper.go
+++ utils/env-helper.go
@@ -3,8 +3,8 @@ package utils
 import "os"
 
 // GetEnv will get the environment value from OS, if empty will use the fallback given.
-// returns string.
-func GetEnv(key string, fallback string) string {
+// returns interface.
+func GetEnv(key string, fallback interface{}) interface{} {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return fallback
--- utils/env-helper_test.go
+++ utils/env-helper_test.go
@@ -6,9 +6,25 @@ import (
 )
 
 func TestGetEnv(t *testing.T) {
-	str := GetEnv("test", "fallback")
+	str := GetEnv("getenvtest", "fallback")
 
 	if len(str) == 0{
 		t.Error("No fallback was supplied for missing os env")
+	}
+
+	if str != "fallback" {
+		t.Error("Different fallback string was given")
+	}
+
+	os.Setenv("ValueTest", "test123")
+
+	str = GetEnv("ValueTest", "")
+
+	if len(str) == 0 {
+		t.Error("Could not retrieve the env")
+	}
+
+	if str != "test123" {
+		t.Error("Wrong env retrieved or fallback used")
 	}
 }
