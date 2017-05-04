ce66d4070b393fd5aa7cc201d5251b6b04ed1ea5
ryan.hancock <ryan.hancock@sipsynergy.co.uk>
2017-05-04 15:41:10 +0100

    DVT-103: changed return comment

--- utils/env-helper.go
+++ utils/env-helper.go
@@ -3,7 +3,7 @@ package utils
 import "os"
 
 // GetEnv will get the environment value from OS, if empty will use the fallback given.
-// returns interface.
+// returns string.
 func GetEnv(key string, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
