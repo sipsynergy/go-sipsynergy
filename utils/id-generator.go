e42b342df9ad6307d40dc54c9e4f03680ad50d5e
luke.rodham <luke.rodham@sipsynergy.co.uk>
2017-08-14 12:27:23 +0100

    fixes validator

--- utils/id-generator.go
+++ utils/id-generator.go
@@ -1,45 +1,45 @@
 package utils
 
 import (
-    "fmt"
-    "math/rand"
-    "regexp"
-    "strconv"
-    "strings"
+	"fmt"
+	"math/rand"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // GenerateHumanID will create a human id.
 // It returns the human id string.
 func GenerateHumanID(prefix string) string {
-    r := GenerateRandomNumericString(10)
-    s := prefix + "-" + r
+	r := GenerateRandomNumericString(10)
+	s := prefix + "-" + r
 
-    return strings.ToUpper(s)
+	return strings.ToUpper(s)
 }
 
 // ValidateHumanID validates the given string.
 // returns true or false.
 func ValidateHumanID(humanID string) bool {
-    re := regexp.MustCompile(`^[A-Z]+-\d+`)
+	re := regexp.MustCompile(`^[A-Z]+(-[A-Z0-9]+){1,2}$`)
 
-    return re.MatchString(humanID)
+	return re.MatchString(humanID)
 }
 
 // Generate random string.
 func GenerateRandomNumericString(length int) string {
-    i := rand.Int63()
-    s := strconv.FormatInt(i, 10)[:length]
+	i := rand.Int63()
+	s := strconv.FormatInt(i, 10)[:length]
 
-    return strings.ToUpper(s)
+	return strings.ToUpper(s)
 }
 
 // Generate random string.
 func GenerateRandomString(length int) (string, error) {
-    n := length
-    b := make([]byte, n)
-    if _, err := rand.Read(b); err != nil {
-        return "", err
-    }
-    s := fmt.Sprintf("%X", b)
-    return s, nil
+	n := length
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	s := fmt.Sprintf("%X", b)
+	return s, nil
 }
