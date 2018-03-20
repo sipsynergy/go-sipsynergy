e2126be56a12aeb18b1745bb851c1bc39d369b79
ryan.hancock <ryan.hancock@sipsynergy.co.uk>
2018-03-20 10:10:14 +0000

    DVT-428: added all strings in slice empty

--- utils/slices_test.go
+++ utils/slices_test.go
@@ -3,17 +3,30 @@ package utils
 import "testing"
 
 const (
-	PREFIX = "TEST"
+	PREFIX_SLICE = "TEST"
 )
 
 func TestIsStringInSlice(t *testing.T) {
-	strList := []string{PREFIX, "TEST2", "TEST3", "TEST4"}
+	strList := []string{PREFIX_SLICE, "TEST2", "TEST3", "TEST4"}
 
-	if !IsStringInSlice(PREFIX, strList) {
+	if !IsStringInSlice(PREFIX_SLICE, strList) {
 		t.Error("Did not find the string correctly")
 	}
 
 	if !IsStringInSlice("FOO", strList) {
+		t.Error("Found string when it should not have.")
+	}
+}
+
+func TestCheckAllStringsEmptyInSlice(t *testing.T) {
+	strList := []string{"TEST1", "", ""}
+	strListEmpty := []string{"", "", ""}
+
+	if CheckAllStringsEmptyInSlice(strList) {
+		t.Error("Should of found a string")
+	}
+
+	if !CheckAllStringsEmptyInSlice(strListEmpty) {
 		t.Error("Found string when it should not have.")
 	}
 }
